823c7c48beafc5ee5e8132ea34972559ba6dcd67
aregdz <144221910+aregdz@users.noreply.github.com>
2023-11-19 11:37:32 +0300

    исправленный вариант

--- PyCharm/zadanie2.py
+++ PyCharm/zadanie2.py
@@ -1,6 +1,8 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import sys
+
 if __name__ == '__main__':
     A = list(map(float, input("Введите элементы списка: ").split()))
     a = int(input("Введите значения a: "))
@@ -10,10 +12,13 @@ if __name__ == '__main__':
     for i, item in enumerate(A):
         if item > 0:
             l = i
+
     s = sum(A[:l+1])
     print(f"Cумма элементов списка, расположенных до последнего положительного элемента - {s}")
+
     d = [i for i in A if abs(i)>a and abs(i)<b]
     print(f"Сжатый список - {d}")
+
     la = len(A)
     ld = len(d)
     while ld != la:
